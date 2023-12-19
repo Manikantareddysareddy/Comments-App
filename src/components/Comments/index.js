29380eeb121fc891b5db400713dcdaf4da51ad33
Manikantareddysareddy <149862326+Manikantareddysareddy@users.noreply.github.com>
2023-12-19 19:32:52 +0530

    Update index.js

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,30 +1,87 @@
 import {Component} from 'react'
 
+import {v4 as uuidv4} from 'uuid'
+
+import {formatDistanceToNow} from 'date-fns'
+
+import CommentItem from '../CommentItem'
+
 import './index.css'
 
 class Comments extends Component {
-  state = {Comment: ''}
+  state = {Comment: '', count: 0, CommentList: [], name: ''}
+
+  likedComment = id => {
+    this.setState(prevState => ({
+      CommentList: prevState.CommentList.map(eachComment => {
+        if (id === eachComment.id) {
+          return {...eachComment, isLiked: !eachComment.isLiked}
+        }
+        return eachComment
+      }),
+    }))
+  }
+
+  deleteComment = id => {
+    this.setState(prevState => ({
+      CommentList: prevState.CommentList.filter(
+        eachComment => eachComment.id !== id,
+      ),
+      count: prevState.count - 1,
+    }))
+  }
+
+  onAddComment = event => {
+    event.preventDefault()
+    const {Comment, name} = this.state
+
+    const newComment = {
+      id: uuidv4(),
+      name,
+      Comment,
+      date: formatDistanceToNow(new Date()),
+      isLiked: false,
+    }
+    this.setState(prevState => ({
+      CommentList: [...prevState.CommentList, newComment],
+      count: prevState.count + 1,
+      name: '',
+      Comment: '',
+    }))
+  }
+
+  onTypeName = event => {
+    this.setState({name: event.target.value})
+  }
+
+  onTypeComment = event => {
+    this.setState({Comment: event.target.value})
+  }
 
   render() {
-    const {Comment} = this.state
+    const {Comment, count, name, CommentList} = this.state
     return (
       <div className="bg-container">
         <h1 className="heading">Comments</h1>
         <div className="Comment-container">
           <div>
             <p className="para">Say something about 4.0 Technologies</p>
-            <form className="form-container">
+            <form className="form-container" onSubmit={this.onAddComment}>
               <input
                 type="text"
                 placeholder="Your Name"
                 className="text-Input"
+                value={name}
+                onChange={this.onTypeName}
               />
               <textarea
                 type="text"
                 rows="6"
                 cols="20"
+                value={Comment}
                 placeholder="Your Comment"
                 className="textArea-Input"
+                onChange={this.onTypeComment}
               >
                 {Comment}
               </textarea>
@@ -39,8 +96,23 @@ class Comments extends Component {
             alt="comments"
           />
         </div>
+        <hr className="horizontal-line" />
+        <p className="para">
+          <span className="span-El">{count}</span> Comments
+        </p>
+        <ul>
+          {CommentList.map(eachComment => (
+            <CommentItem
+              commentItem={eachComment}
+              key={eachComment.id}
+              likedComment={this.likedComment}
+              deleteComment={this.deleteComment}
+            />
+          ))}
+        </ul>
       </div>
     )
   }
 }
 export default Comments
+
